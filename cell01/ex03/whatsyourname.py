408dd122139726671e2b466a697810717bf83566
prawa26 <68030216@kmitl.ac.th>
2025-06-16 14:23:19 +0000

    ex03 c1

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,14 @@
+name = 'prawa'
+last_name = 'sayowan'
+
+s = f"Hey, what's your first name? : {name}"
+
+print(s)
+
+o = f"And your last name? : {last_name}"
+
+print(o)
+
+y = f"Well, pleased to meet you, {name} {last_name}."
+
+print(y)
