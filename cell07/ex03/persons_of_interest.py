36bbdd3df4efda1cdd717c5b342b31e513c8f588
prawa26 <68030216@kmitl.ac.th>
2025-06-18 15:01:26 +0000

    03

---
+++ cell07/ex03/persons_of_interest.py
@@ -0,0 +1,11 @@
+def famous_births(figures):
+    sorted_figures = sorted(figures.values(), key=lambda person: int(person["date_of_birth"]))
+    for person in sorted_figures:
+        print(f'{person["name"]} is a great scientist born in {person["date_of_birth"]}.')
+scientists = {
+    "a": { "name": "G", "date_of_birth": "3333" },
+    "b": { "name": "E", "date_of_birth": "6666" },
+    "c": { "name": "N", "date_of_birth": "1000" },
+    "d": { "name": "O", "dath_of_birth": "2007" }
+}
+famous_births(scientists)
