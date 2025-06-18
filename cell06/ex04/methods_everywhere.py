0e9e9319934fcd287a1b3035317ee67dc57cb35c
prawa26 <68030216@kmitl.ac.th>
2025-06-18 12:58:24 +0000

    04

--- cell06/ex04/methods_everywhere.py
+++ cell06/ex04/methods_everywhere.py
@@ -4,4 +4,12 @@ def shrink(i):
 def enlarge(i):
     print(i + 'z' * (8 - len(i)))
 if len(sys.argv) < 2:
-    print
+    print("none")
+else:
+    for arg in sys.argv[1:]:
+        if len(arg) > 8:
+            shrink(arg)
+        elif len(arg) < 8:
+            enlarge(arg)
+        else:
+            print(arg)
