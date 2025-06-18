d3086249efb9ab18a5bf962e1d1ce1d0d8b268d5
prawa26 <68030216@kmitl.ac.th>
2025-06-18 02:09:02 +0000

    c5 ex03

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -6,8 +6,6 @@ original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array = list({num + 2 for num in original_array if num + 2 > 5})
 
 
-new_array.sort()
-
 
 print( original_array)
 print( new_array)
